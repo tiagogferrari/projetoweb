7f9e2d75f2a752fd5e4ee9befab15974334387d8
Tiago Garcez Ferrari <tiagoggferrari@gmail.com>
2023-06-24 17:54:45 -0300

    Criação das API que terão as rotas

--- control/Installapi.js
+++ control/Installapi.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const sequelize = require("../helpers/bd")
-const UserModel = require('../model/usuario')
+
+const UsuarioModel = require('../model/Usuario')
+const AutorModel = require('../model/Autor')
+const CategoriaModel = require('../model/Categoria')
+const CursoModel = require('../model/Curso')
+const UsuariocursoModel = require('../model/Usuariocurso')
 
 router.get('/', async (req, res) => {
     await sequelize.sync({ force: true })
@@ -9,3 +14,4 @@ router.get('/', async (req, res) => {
 })
 
 module.exports = router
+
