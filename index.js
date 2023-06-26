c967c927bce60eca384d5f5cd9cda308814e4da0
Tiago Garcez Ferrari <tiagoggferrari@gmail.com>
2023-06-26 18:47:05 -0300

    Arrumando os app.use e requires

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ require("dotenv").config()
 const sequelize = require("./helpers/bd")
 
 const app = express()
+
 app.use(express.json())
-app.use('/install', require("./control/Installapi"))
+app.use('/install', require("./control/InstallAPI"))
+app.use('/administrador', require("./control/AdministradorAPI"))
+app.use('/autor', require("./control/AutorAPI"))
+app.use('/categoria', require("./control/CategoriaAPI"))
+app.use('/curso', require("./control/CursoAPI"))
+app.use('/login', require("./control/LoginAPI"))
+app.use('/usuario', require("./control/UsuarioAPI"))
+app.use('/usuariocurso', require("./control/UsuariocursoAPI"))
 
 app.listen(3000, () => {
     console.log('Working... http://localhost:3000')
 })
+
